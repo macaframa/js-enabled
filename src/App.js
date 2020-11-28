baf7e25cd5a945057802d1d66a49e44d34e46ddc
Divyesh Parmar <divyeshpar2496@gmail.com>
2020-11-29 01:58:38 +0530

    first commit with food list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [firstRun, updateFirstRun] = useState(true);
+  const [food, updateFood] = useState([]);
+
+  // const getFood = async () => {}
+
+  useEffect(() => {
+    if (firstRun) {
+      getFood();
+      updateFirstRun(false);
+    }
+
+    async function getFood() {
+      try {
+        const fetchedFood = await fetch('./food.json');
+        console.log('stil promise', fetchedFood);
+        const food = await fetchedFood.json();
+        console.log('fetched food', food);
+        updateFood(food.food);
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='food-list'>
+        {food &&
+          food.map(({ name, url, description, currency, price }, idx) => (
+            <div key={`food-${idx}`} className='food-card'>
+              {idx + 1}
+              <p>{name}</p>
+              <span>♥</span>
+              <span className='food-image'>
+                <img src={url} alt='food_image' />
+              </span>
+              <span className='food-info'>
+                <p>{description}</p>
+                <p>{price}</p>
+              </span>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
